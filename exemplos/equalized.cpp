ba1bb1296888fe330323a62368b254b8d5e895b1
barbaragabriella <barbara_gabriellajc@hotmail.com>
2017-09-05 19:45:11 -0300

    at

--- exemplos/equalized.cpp
+++ exemplos/equalized.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
@@ -6,105 +5,95 @@ using namespace cv;
 using namespace std;
 
 int main(int argc, char** argv){
-  Mat imagem, equalizar;
+  int key;
+  Mat image, equalizar;
 
-  // carrega a imagem
-  imagem = imread( argv[1], 1 );
+  int width, height;
 
-  /// converte para tons de cinza
-  cvtColor( imagem, imagem, CV_BGR2GRAY );
+  VideoCapture cap;
 
-  /// Aplica a equalização
-  equalizeHist( imagem, equalizar);
+  vector<Mat> planes;
 
-  /// Mostra os resultados
-  namedWindow( "imagem original", CV_WINDOW_AUTOSIZE );
-  namedWindow( "imagem equalizada", CV_WINDOW_AUTOSIZE );
+  Mat histR, histE;
 
-  imshow( "imagem original", imagem );
-  imshow( "imagem equalizada", equalizar );
+  int nbins = 64;
 
+  float range[] = {0, 256};
 
-  imwrite("nequalizado.png", imagem);
-  imwrite("equalizado.png", equalizar);
+  const float *histrange = { range };
 
-  // espera até que o usuário finaliza
-  waitKey(0);
+  bool uniform = true;
+
+  bool acummulate = false;
+
+  cap.open(0);
+  
+  if(!cap.isOpened()){
+    cout << "cameras indisponiveis";
+    return -1;
+  }
+
+  width  = cap.get(CV_CAP_PROP_FRAME_WIDTH);
+  height = cap.get(CV_CAP_PROP_FRAME_HEIGHT);
+
+  cout << "largura = " << width << endl;
+  cout << "altura  = " << height << endl;
+
+  int histw = nbins, histh = nbins/2;
+
+  Mat histImgR(histh, histw, CV_BGR2GRAY, Scalar(0,0,0));
+  Mat histImgE(histh, histw, CV_BGR2GRAY, Scalar(0,0,0));
+
+  while(1){
+    cap >> image;
+    //split (image, planes);
+
+    /// converte para tons de cinza
+    cvtColor( image, image, CV_BGR2GRAY );
+
+    calcHist(&image, 1, 0, Mat(), histR, 1,
+             &nbins, &histrange,
+             uniform, acummulate);
+
+    normalize(histR, histR, 0, histImgR.rows, NORM_MINMAX, -1, Mat());
+
+    histImgR.setTo(Scalar(0));
+    
+    for(int i=0; i<nbins; i++){
+      line(histImgR,
+           Point(i, histh),
+           Point(i, histh-cvRound(histR.at<float>(i))),
+           Scalar(0, 0, 255), 1, 8, 0);
+    }
+
+    equalizeHist( image, equalizar);
+
+    calcHist(&equalizar, 1, 0, Mat(), histE, 1,
+               &nbins, &histrange,
+               uniform, acummulate);
+
+    normalize(histE, histE, 0, histImgE.rows, NORM_MINMAX, -1, Mat());
+    histImgE.setTo(Scalar(0));
+
+    for(int i=0; i<nbins; i++){
+       line(histImgE, Point(i, histh),
+         Point(i, cvRound(histE.at<float>(i))),
+         Scalar(255), 1, 8, 0);
+    }
+
+    histImgR.copyTo(image(Rect(0, 0 ,nbins, histh)));
+    histImgE.copyTo(equalizar(Rect(0, 0 ,nbins, histh)));
+
+    imshow("image", image);
+    imshow("Equalizado", equalizar);
+
+    imwrite("nequalizado.png", image);
+    imwrite("equalizado.png", equalizar);
+    key = (char) waitKey(10);
+    if(key == 27) {
+      break;
+    }
+  }
 
   return 0;
 }
-
-// #include "opencv2/highgui/highgui.hpp"
-// #include "opencv2/imgproc/imgproc.hpp"
-// #include <iostream>
-// #include <stdio.h>
-
-// using namespace std;
-// using namespace cv;
-
-// /**
-//  * @function main
-//  */
-// int main( int argc, char** argv )
-// {
-//   Mat src, dst;
-
-//   /// Load image
-//   src = imread( argv[1], 1 );
-
-//   if( !src.data )
-//     { return -1; }
-
-//   /// Separate the image in 3 places ( B, G and R )
-//   vector<Mat> bgr_planes;
-//   split( src, bgr_planes );
-
-//   /// Establish the number of bins
-//   int histSize = 256;
-
-//   /// Set the ranges ( for B,G,R) )
-//   float range[] = { 0, 256 } ;
-//   const float* histRange = { range };
-
-//   bool uniform = true; bool accumulate = false;
-
-//   Mat b_hist, g_hist, r_hist;
-
-//   /// Compute the histograms:
-//   calcHist( &bgr_planes[0], 1, 0, Mat(), b_hist, 1, &histSize, &histRange, uniform, accumulate );
-//   calcHist( &bgr_planes[1], 1, 0, Mat(), g_hist, 1, &histSize, &histRange, uniform, accumulate );
-//   calcHist( &bgr_planes[2], 1, 0, Mat(), r_hist, 1, &histSize, &histRange, uniform, accumulate );
-
-//   // Draw the histograms for B, G and R
-//   int hist_w = 512; int hist_h = 400;
-//   int bin_w = cvRound( (double) hist_w/histSize );
-
-//   Mat histImage( hist_h, hist_w, CV_8UC3, Scalar( 0,0,0) );
-
-//   /// Normalize the result to [ 0, histImage.rows ]
-//   normalize(b_hist, b_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
-//   normalize(g_hist, g_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
-//   normalize(r_hist, r_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
-
-//   /// Draw for each channel
-//   for( int i = 1; i < histSize; i++ )
-//   {
-//       line( histImage, Point( bin_w*(i-1), hist_h - cvRound(b_hist.at<float>(i-1)) ) ,
-//                        Point( bin_w*(i), hist_h - cvRound(b_hist.at<float>(i)) ),
-//                        Scalar( 255, 0, 0), 2, 8, 0  );
-//       line( histImage, Point( bin_w*(i-1), hist_h - cvRound(g_hist.at<float>(i-1)) ) ,
-//                        Point( bin_w*(i), hist_h - cvRound(g_hist.at<float>(i)) ),
-//                        Scalar( 0, 255, 0), 2, 8, 0  );
-//       line( histImage, Point( bin_w*(i-1), hist_h - cvRound(r_hist.at<float>(i-1)) ) ,
-//                        Point( bin_w*(i), hist_h - cvRound(r_hist.at<float>(i)) ),
-//                        Scalar( 0, 0, 255), 2, 8, 0  );
-//   }
-
-//   /// Display
-//   namedWindow("calcHist Demo", CV_WINDOW_AUTOSIZE );
-//   imshow("calcHist Demo", histImage );
-
-//   waitKey(0);
-
-//   return 0;
-// }
